859fc00a3d9783a93a536544b1985d9da7ab8060
Jonathan Bergson <contato@bergson.me>
2022-09-01 17:09:34 -0300

    chore: finalizado layout do site e listagem de posts

---
+++ src/components/IframeItchIO.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="me-Iframe-ItchIO">
+    <iframe
+      :src="src"
+      width="552"
+      height="167"
+      frameborder="0"
+    >
+      <a :href="linkUrl">
+        <slot>{{ linkLabel }}</slot>
+      </a>
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgColor: { type: String, default: "161a1d" },
+    borderColor: { type: String, default: "161a1d" },
+    borderWidth: { type: String, default: "0" },
+    textColor: { type: String, default: "d6d6d6" },
+    hasLink: { type: String, default: "true" },
+    id: { type: String, default: "" },
+    buttonColor: { type: String, default: "c71054" },
+    linkUrl: { type: String, default: "https://jonathanbergson.itch.io/" },
+    linkLabel: { type: String, default: "Games by Jonathan Bergson" },
+  },
+  computed: {
+    src() {
+      const params = [
+        `linkback=${this.hasLink}`,
+        `bg_color=${this.bgColor}`,
+        `fg_color=${this.textColor}`,
+        `link_color=${this.buttonColor}`,
+        `border_color=${this.borderColor}`,
+        `border_width=${this.borderWidth}`,
+      ].join("&")
+      return `https://itch.io/embed/${this.id}?${params}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.me-Iframe-ItchIO {
+  margin: 0 -12px;
+
+  & > iframe {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/IframeYouTube.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="me-Iframe-YouTube">
+    <iframe
+      width="560"
+      height="315"
+      :src="src"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: { type: String, default: "" }
+  },
+  computed: {
+    src() {
+      return `https://www.youtube.com/embed/${this.id}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.me-Iframe-YouTube {
+  background-color: #1f2328;
+  height: 0;
+  margin-bottom: 1em;
+  padding-bottom: 56.25%;
+  position: relative;
+
+  & > iframe {
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+  }
+}
+</style>
